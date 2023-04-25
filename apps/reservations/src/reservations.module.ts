a85b259e219728e84a9c0ae4936f1f388ac7a169
Francisco <franciscoctism@gmail.com>
2023-04-24 22:15:40 -0300

    chore: reservation data validation and log config

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,13 +4,15 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common/database/database.module';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { LoggerModule } from '@app/common/logger/logger.module';
 
 @Module({
   imports: [
     DatabaseModule, 
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema }
-    ])
+    ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository]
