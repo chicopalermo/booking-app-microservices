052c5977de1ffde3776ef441e810e08ffc4ae036
Francisco <franciscoctism@gmail.com>
2023-04-30 16:20:41 -0300

    config: jwt configuration and envs microservices refactor

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common/database/database.module';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { LoggerModule } from '@app/common/logger/logger.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -13,6 +15,13 @@ import { LoggerModule } from '@app/common/logger/logger.module';
       { name: ReservationDocument.name, schema: ReservationSchema }
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required()
+      })
+    })
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository]
