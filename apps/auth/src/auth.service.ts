1b090fd53dfb0ba02016493a06e5a04b3c58141f
Francisco <franciscoctism@gmail.com>
2023-06-02 11:49:39 -0300

    feat: jwt strategy implementation

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -3,6 +3,7 @@ import { UserDocument } from './users/models/users.schema';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
+import { TokenPayload } from './interfaces/token-payload.interfaces';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,7 @@ export class AuthService {
   ) {}
 
   async login(user: UserDocument, response: Response) {
-    const tokenPayload  = { 
+    const tokenPayload: TokenPayload  = { 
       userId: user._id.toHexString(),
     };
 
